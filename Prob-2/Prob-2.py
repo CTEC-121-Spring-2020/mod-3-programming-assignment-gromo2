208fb099702f8a06303b19229c4b8b9d3f9424eb
Garrett <g.romo2@students.clark.edu>
2020-05-06 14:25:29 -0700

    Version 2: Doing this to save, program wont save otherwise

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -6,15 +6,34 @@
 
 '''
 #Inputs: Asks user for name, hourly wage, and hours
-#Process: Calculates tax, insurance, and overtime wages, if applicable
-#Outputs: Total paycheck
+#Process: Calculates tax, insurance, and overtime wages, if applicable, along with total take-home paycheck.
+#Outputs: Completed paycheck
 '''
 
 def main():
     employeeName = input("\nEnter Name: ")
+    print()
+    hourlyWages = int(input("Input Hourly Wages: "))
+    print()
+    workHours = int(input("Input Weekly Hours: "))
+    print()
+
+    if workHours > 40:
+        overtimeWages = (workHours - 40) * (hourlyWages * 1.5)
+    else: overtimeWages = 0
+    normalWages = (hourlyWages * workHours) #Should print like ("$", normalWages), etc
+    taxWithholding = normalWages / 5
+    insuranceWithholding = normalWages / 10
+    totalWages = overtimeWages + normalWages
+    totalPay = totalWages - (taxWithholding + insuranceWithholding)
+
+    print("Name:", employeeName)
+    print("Regular Wages:", normalWages)
+    print("Overtime Wages:", overtimeWages)
+    print("Total Wages:", totalWages)
+    print("Tax Withholding:", taxWithholding)
+    print("Insurance Withholding:", insuranceWithholding)
+    print("Total Pay:", totalPay)
     
-    print(employeeName)
-
-
 
 main()
